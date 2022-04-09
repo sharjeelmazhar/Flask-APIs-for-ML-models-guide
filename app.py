fe213bc8a560b5c6d141c0287d2d70d1478fb61e
Sharjeel M <73254881+sharjeelmazhar@users.noreply.github.com>
2022-04-09 23:37:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -20,13 +20,13 @@ def predict_named():
     v_6=int(request.args.get('v6'))
     v_7=int(request.args.get('v7'))
     prediction = model.predict([[v_1,v_2,v_3,v_4,v_5,v_6,v_7]])
-    return jsonify({'Prediction ': int(prediction) })
+    return jsonify({'Prediction': int(prediction) })
 
 @app.route('/predictUnnamed/<int:v_1>/<int:v_2>/<int:v_3>/<int:v_4>/<int:v_5>/<int:v_6>/<int:v_7>',methods = ['GET','POST'])
 def predict_unnamed(v_1,v_2,v_3,v_4,v_5,v_6,v_7):
     """This function gets the unnamed args from the api and predicts the label"""
     prediction = model.predict([[v_1,v_2,v_3,v_4,v_5,v_6,v_7]])
-    return jsonify({'Prediction ': int(prediction) })
+    return jsonify({'Prediction': int(prediction) })
 
 @app.route('/predictBodyNamed',methods = ['GET','POST'])
 def predict_body_named():
@@ -52,7 +52,7 @@ def predict_body_named():
     print(v1,v2,v3,v4,v5,v6,v7)
     # prediction = model.predict(query_df)
     prediction = model.predict([[v1,v2,v3,v4,v5,v6,v7]])
-    return jsonify({'Prediction ': int(prediction) })
+    return jsonify({'Prediction': int(prediction) })
 
 if __name__ == '__main__':
     app.run(debug=True)
